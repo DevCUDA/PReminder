93fe776f69f9e9f5ab9703ee552ebe2d4786ecf8
ThisCuda <this.cuda@gmail.com>
2016-06-20 02:35:46 +0300

    Valuables #1

---
+++ PReminder/app/src/main/java/com/devcuda/preminder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.devcuda.preminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by thisc on 20.06.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
