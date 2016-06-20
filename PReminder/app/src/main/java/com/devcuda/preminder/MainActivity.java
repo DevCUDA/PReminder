cd27d8e398983ab94c6826bf20b16fb93b493ec2
ThisCuda <this.cuda@gmail.com>
2016-06-20 15:55:07 +0300

    Valuables #1

--- PReminder/app/src/main/java/com/devcuda/preminder/MainActivity.java
+++ PReminder/app/src/main/java/com/devcuda/preminder/MainActivity.java
@@ -1,16 +1,33 @@
 package com.devcuda.preminder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by thisc on 20.06.2016.
- */
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+
     }
-}
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+        }
+    }
